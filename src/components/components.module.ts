83cf0ee2286573f207af93930401235992f71a95
Bosi Lin <terrence.lin@ustoshop.com>
2018-09-11 14:24:40 -0700

    deleted unnecessary module

--- src/components/components.module.ts
+++
@@ -1,8 +0,0 @@
-import { NgModule } from '@angular/core';
-import { UserComponent } from './user/user';
-@NgModule({
-	declarations: [UserComponent],
-	imports: [],
-	exports: [UserComponent]
-})
-export class ComponentsModule {}
