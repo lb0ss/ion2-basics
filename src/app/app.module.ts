8e8d7ae71074572db70b061bf18ad2978d73d59f
Bosi Lin <terrence.lin@ustoshop.com>
2018-09-11 14:23:32 -0700

    enabled basic nav features

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,13 +8,17 @@ import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
 import { UsersPage } from '../pages/users/users';
 import { UserPage } from '../pages/users/user/user';
+import { ShopPage } from '../pages/shop/shop';
+import { BuyoutPage } from '../pages/buyout/buyout';
 
 @NgModule({
   declarations: [
     MyApp,
     HomePage,
     UsersPage,
-    UserPage
+    UserPage,
+    ShopPage,
+    BuyoutPage
   ],
   imports: [
     BrowserModule,
@@ -24,7 +28,10 @@ import { UserPage } from '../pages/users/user/user';
   entryComponents: [
     MyApp,
     HomePage,
-    UsersPage
+    UsersPage,
+    UserPage,
+    ShopPage,
+    BuyoutPage
   ],
   providers: [
     StatusBar,
